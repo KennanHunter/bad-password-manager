5d5c4eeefd14fa33bf0f6895eb19a8024fa0a7a7
Kennan Hunter <KennanHunter5@gmail.com>
2021-06-08 16:22:33 -0400

    E

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(decl_macro)]
 
 use rocket::{get, routes};
-
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
-}
+use rocket_contrib::serve::StaticFiles;
+use bad_pass_manager::fetch_vault;
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/", StaticFiles::from("/app/"))
+        .mount("/api/", routes![fetch_vault])
+        .launch();
 }
