92ebda44a723a18cbf2217fb014be17d0888cacd
Kennan Hunter <kennanhunter5@gmail.com>
2021-08-04 01:02:48 -0400

    docker working poggersssss

--- api/src/main.rs
+++ api/src/main.rs
@@ -8,8 +8,8 @@ use rocket;
 fn main() {
     rocket::ignite()
         // .mount("/", StaticFiles::from("/app/")) // switched to handling it with nginx
-        .mount("/vault", vault::get_routes())
-        .mount("/user", user::get_routes())
+        .mount("/api/vault", vault::get_routes())
+        .mount("/api/user", user::get_routes())
 
         .launch();
 }
